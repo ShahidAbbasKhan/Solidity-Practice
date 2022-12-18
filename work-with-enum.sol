a0120c3ce4a4a9fd38a504bb3a692849198ef75c
ShahidAbbasKhan <developershahid297@gmail.com>
2022-12-18 12:27:59 +0500

    Updating Enum Contract

--- work-with-enum.sol
+++ work-with-enum.sol
@@ -2,5 +2,28 @@
 
 pragma solidity ^0.8.9;
 contract Enums{
-    
+    enum Status{isPurchased,amountPaid,sentForDelivery,isReceived}
+    Status status;
+    function  purchased() public returns(Status){
+        status=Status.isPurchased;
+        return status;
+    }
+    function  paidAmount() public returns(Status){
+        status=Status.amountPaid;
+        return status;
+    }
+    function  sentForDelivery() public returns(Status){
+        status=Status.sentForDelivery;
+        return status;
+    }
+    function  isReceived() public returns(Status){
+        status=Status.isReceived;
+        return status;
+    }
+    function payNow() payable public {
+        require(msg.value>0.005 ether,"pay amount 0.005 ether");
+        require(status==Status.isPurchased,"Please Purchase Item ");
+    }
+
+
 }
